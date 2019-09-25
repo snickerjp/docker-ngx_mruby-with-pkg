5432d669fecd3a93768cf98748909a42f1bb0f45
snickerjp <snickerjp@gmail.com>
2019-09-25 19:10:26 +0900

    mod script

--- 02_build_deb_pkg.sh
+++ 02_build_deb_pkg.sh
@@ -1,10 +1,23 @@
 #/bin/bash
 
+function my_error() {
+    # エラー処理
+    exit 1
+}
+
 # Copy build_config.rb
-cp ngx_mruby/build_config.rb ngx_mruby-package-builder/ngx_mruby/build_config.rb \
+
+function copy_build_config() {
+    cp ngx_mruby/build_config.rb ngx_mruby-package-builder/ngx_mruby/build_config.rb
+}
 
 # build deb for ubuntu1804
-&& cd ngx_mruby-package-builder \
-&& docker-compose -p ngxmrubypackagebuilder build ubuntu1804 \
-&& docker images \
-&& cd ..
+function build_deb() {
+    cd ngx_mruby-package-builder
+    docker-compose -p ngxmrubypackagebuilder build ubuntu1804
+    docker images
+    cd ..
+}
+
+# main
+copy_build_config && build_deb || my_error
