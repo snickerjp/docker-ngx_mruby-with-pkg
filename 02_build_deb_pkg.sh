2ae5b954991c99cafe1328860078097f9bef3ce4
snickerjp <snickerjp@gmail.com>
2019-09-25 18:59:02 +0900

    エラーで終わるように、ワンライナーにした

--- 02_build_deb_pkg.sh
+++ 02_build_deb_pkg.sh
@@ -1,11 +1,10 @@
 #/bin/bash
 
 # Copy build_config.rb
-cp ngx_mruby/build_config.rb ngx_mruby-package-builder/ngx_mruby/build_config.rb
+cp ngx_mruby/build_config.rb ngx_mruby-package-builder/ngx_mruby/build_config.rb \
 
 # build deb for ubuntu1804
-cd ngx_mruby-package-builder
-docker-compose -p ngxmrubypackagebuilder build ubuntu1804
-
-docker images
-cd ..
+&& cd ngx_mruby-package-builder \
+&& docker-compose -p ngxmrubypackagebuilder build ubuntu1804 \
+&& docker images \
+&& cd ..
--- 03_build_docker-ngx_mruby.sh
+++ 03_build_docker-ngx_mruby.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 # build deb for ubuntu1804
-docker build -t snickerjp/docker-ngx_mruby-with-pkg .
-docker images
+docker build -t snickerjp/docker-ngx_mruby-with-pkg . \
+&& docker images
